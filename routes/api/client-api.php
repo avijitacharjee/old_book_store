d8f3dd1ac3738de9be30a515b9e0997193ff1a26
iammeskat <meskatcse@gmail.com>
2021-08-02 16:04:59 +0600

    ..

--- routes/api/client-api.php
+++ routes/api/client-api.php
@@ -66,7 +66,7 @@ Route::group(['prefix' => 'rcs'], function(){
     Route::get('/ads/{loc_slug}/{cat_slug}', [AdsController::class, 'getAdsByLocAndCat']); // get ads of a specific location & category (pp20)
 
     // User profile with ads
-    Route::get('/user/{user_id}', [UserController::class, 'getUser']); // get an  user profile
+    Route::get('/user/{user_id}', [UserController::class, 'getUser']); // get a user profile
     Route::get('/user/{user_id}/ads', [UserController::class, 'getAds']); // get all Ads of a specific user
 
 });
